2a4c0ae67697b22a99dc4b6d7d82f7aec1d94bfb
Niv Suroghon <nsuroghon@gmail.com>
2021-01-06 21:16:03 -0500

    install console.table and inserted data to db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const Choice = require('inquirer/lib/objects/choice');
+const consoleTable = require('console.table');
 
 //Create Connection
 const connection = mysql.createConnection({
@@ -18,5 +20,25 @@ connection.connect((err) => {
   if(err){
     throw err;
   }
-  console.log("Connected as id " + connection.threadId)
+  console.log("Connected as id " + connection.threadId);
+  init();
 });
+
+function init(){
+  inquirer.prompt({
+    type: 'list',
+    name: 'startPrompt',
+    message: 'What would you like to do?',
+    choices: ['View All Employees', 'View All Departments', 'View All Employee Roles', 'Add Employee', 'Add Department', 'Add role', 'Update Employee Role']
+  })
+  .then(function (response){
+    switch (response.start) {
+      
+      case "View All Employees":
+        viewEmployees();
+        break;
+      
+    }
+  })
+};
+
--- seed.sql
+++ seed.sql
@@ -1,1 +1,15 @@
--- inserting our data into the schema 
+USE employeeDB
+
+INSERT INTO department(name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesperson', 80000, 1), ('Sales Lead', 100000, 1), ('Software Engineer', 150000, 2), ('Lead Software Engineer', 180000, 2), ('Accountant', 125000, 3), ('Finance Manager', 150000, 3), ('Legal', 200000, 4), ('Legal Manager', 250000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('Lebron', 'James','1', null), 
+('Tom', 'Brady', 2, null), 
+('Kevin', 'Durant', 3, null), 
+('Stephen', 'Curry', 4, null),
+('Serena', 'Williams', 5, null), 
+('Tiger', 'Woods', 6, null);
