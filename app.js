10ea88826bf76df65c29cb03b43833a0382b72d4
Niv Suroghon <nsuroghon@gmail.com>
2021-01-08 23:04:08 -0500

    password

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const connection = mysql.createConnection({
     // Your port; if not 3306
     port: 3306,
     // Your username
-    user: 'niv',
+    user: 'root',
     // Be sure to update with your own MySQL password!
-    password: 'suroghon',
+    password: '',
     database: 'employeeDB',
   });
 
